2c2f4498d9f4746a540cb55776cc089a61688659
Brandon BC <brand.bc0@gmail.com>
2024-10-29 20:21:10 -0600

    Improved command reading system

--- troll-board.py
+++ troll-board.py
@@ -1,22 +1,48 @@
 import pyperclip as cb
 
-text = "funcionando"
-oldWord = ";"
-newWord = " miuau"
-cb.copy(text)
+text = "funcionando" # in this variable we're going to save the clipboard value
+oldWord = ";" # is the word we are going to replace
+newWord = "miau" # is the word that will replace the old word
+
+# this is the help message that will be explained to the users
 helpMsg = "\'endtroll\' = termina el programa\n\'oword:x\' = cambia la palabra a remplazar por x y la palabra nueva es igual pero con nword\n\'info\' = muestra las palabras"
+
+def evaluateCommand (text) :
+    sections = text.split(":")
+    commandKey = sections[0]
+    commandValue = sections[1] if len(sections) > 1 else ""
+
+    if (not commandKey in commands): return False
+
+    commands[commandKey](commandValue)
+    return True
+
+def setOldWord (value) :
+    global oldWord
+    oldWord = value
+
+def setNewWord (value) :
+    global newWord
+    newWord = value
+
+def showInfo (value) :
+    cb.copy("palabra a remplazar: " + oldWord + "\npalabra nueva: " + newWord)
+
+def showHelp (value) :
+    cb.copy("hola guapote, los comandos funcionan copiandolos\n" + helpMsg)
+
+commands = {
+    "oword": setOldWord,
+    "nword": setNewWord,
+    "info": showInfo,
+    "help": showHelp
+}
 
 while (cb.paste() != "endtroll") :
     cb.waitForNewPaste()
     text = cb.paste()
-    if (text[:6] == "oword:") :
-        oldWord = text[6:]
-    elif (text[:6] == "nword:") :
-        newWord = text[6:]
-    elif (text[:4] == "info") :
-        cb.copy("palabra a remplazar: " + oldWord + "\npalabra nueva: " + newWord)
-    elif (text[:4] == "help") :
-        cb.copy("hola guapote, los comandos funcionan copiandolos\n" + helpMsg)
-    else :
+
+    # if the clipboard text is not a command we're going to replace the words
+    if not evaluateCommand(text) :
         text = text.replace(oldWord, newWord)
         cb.copy(text)
